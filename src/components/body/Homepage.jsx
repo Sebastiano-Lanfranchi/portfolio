61cbe0d073750262770496ff8a53c1c9da5af49a
Sebastiano Lanfranchi <sebastiano.lanfranchi@ff3d.com>
2022-04-22 18:02:17 +0200

    aggiunta immagini jumbotron

--- src/components/body/Homepage.jsx
+++ src/components/body/Homepage.jsx
@@ -1,13 +1,29 @@
-import './Homepage.css'
-
-
+import './Homepage.css';
+import frntlg from '../../frntlg.png';
+import cmplg from '../../cmplg.png';
+import ptglg from '../../ptglg.png';
+import React, { useState } from 'react';
 
 function Homepage() {
+
+    const [imgHome, setImgHome] = React.useState(<div className='max-w-xl relative left-1/4 bottom-24'><img src={frntlg} /></div>);
+
+    function changeImgJumbo(item) {
+        if (item == 'dev') {
+            return setImgHome(<div className='max-w-xl relative left-1/4 bottom-24'><img src={frntlg} /></div>)
+        }
+        else if (item == 'composer') {
+            return setImgHome(<div className='max-w-xl relative left-1/4 bottom-24'><img src={cmplg} /></div>)
+        }else{
+            return setImgHome(<div className='max-w-xl relative left-1/4 bottom-24'><img src={ptglg} /></div>)
+        }
+    }
+
     return (
-        <div className="homepagePosition w-full">
+        <div className="homepagePosition w-full" >
             <div className='flex justify-between w-1/2 center-div'>
                 <div className='flex flex-col text-pink-50'>
-                    <span className='flex justify-between text-center mb-20'>
+                    <span className='flex justify-between text-center mb-24 cursor-pointer' onClick={() => changeImgJumbo('dev')}>
                         <div className='containerEstKey'>
                             <div className='containerIntKey'>
                                 <div className='containerLetter rainbow rainbow_text_animated'>Q</div>
@@ -15,7 +31,7 @@ function Homepage() {
                         </div>
                         <div className="textVerticalAlign titleFont text-5xl ml-14 mt-2">Front-End Dev</div>
                     </span>
-                    <span className='flex justify-between mb-20'>
+                    <span className='flex justify-between mb-24 cursor-pointer' onClick={() => changeImgJumbo('composer')}>
                         <div className='containerEstKey'>
                             <div className='containerIntKey'>
                                 <div className='containerLetter rainbow rainbow_text_animated'>W</div>
@@ -23,7 +39,7 @@ function Homepage() {
                         </div>
                         <div className="textVerticalAlign titleFont text-5xl ml-14 mt-2">Composer</div>
                     </span>
-                    <span className='flex justify-between'>
+                    <span className='flex justify-between' onClick={() => changeImgJumbo('photographer')}>
                         <div className='containerEstKey'>
                             <div className='containerIntKey'>
                                 <div className='containerLetter rainbow rainbow_text_animated pl-1'>E</div>
@@ -32,6 +48,7 @@ function Homepage() {
                         <div className="textVerticalAlign ml-12 titleFont text-5xl ml-14 mt-2">Photographer</div>
                     </span>
                 </div>
+                {imgHome}
             </div>
         </div>
     )
