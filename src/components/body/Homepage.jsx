5fe15ebd2d2cee47a78749af06e5af63ee273834
Sebastiano Lanfranchi <sebastiano.lanfranchi@ff3d.com>
2022-04-28 10:58:37 +0200

    cambio stile portfolio

--- src/components/body/Homepage.jsx
+++ src/components/body/Homepage.jsx
@@ -1,54 +1,35 @@
 import './Homepage.css';
-import frntlg from '../../frntlg.png';
-import cmplg from '../../cmplg.png';
-import ptglg from '../../ptglg.png';
+import frntlg from '../../frntlg.jpg';
+import cmplg from '../../cmplg.jpg';
+import ptglg from '../../ptglg.jpg';
 import React, { useState } from 'react';
 
 function Homepage() {
 
-    const [imgHome, setImgHome] = React.useState(<div className='max-w-xl relative left-1/4 bottom-24'><img src={frntlg} /></div>);
+    const [imgHome, setImgHome] = useState(<div className='max-w-xl relative left-1/4 bottom-24'><img src={frntlg} alt="frn" /></div>);
 
     function changeImgJumbo(item) {
-        if (item == 'dev') {
-            return setImgHome(<div className='max-w-xl relative left-1/4 bottom-24'><img src={frntlg} /></div>)
-        }
-        else if (item == 'composer') {
-            return setImgHome(<div className='max-w-xl relative left-1/4 bottom-24'><img src={cmplg} /></div>)
-        }else{
-            return setImgHome(<div className='max-w-xl relative left-1/4 bottom-24'><img src={ptglg} /></div>)
+        if (item === 'dev') {
+            return setImgHome(<div className='max-w-xl relative left-1/4 bottom-24'><img src={frntlg} alt="frn" /></div>)
+        } else if (item === 'composer') {
+            return setImgHome(<div className='max-w-xl relative left-1/4 bottom-24'><img src={cmplg} alt="cmp" /></div>)
+        } else {
+            return setImgHome(<div className='max-w-xl relative left-1/4 bottom-24'><img src={ptglg} alt="ptg" /></div>)
         }
     }
 
     return (
         <div className="homepagePosition w-full" >
-            <div className='flex justify-between w-1/2 center-div'>
-                <div className='flex flex-col text-pink-50'>
-                    <span className='flex justify-between text-center mb-24 cursor-pointer' onClick={() => changeImgJumbo('dev')}>
-                        <div className='containerEstKey'>
-                            <div className='containerIntKey'>
-                                <div className='containerLetter rainbow rainbow_text_animated'>Q</div>
-                            </div>
-                        </div>
-                        <div className="textVerticalAlign titleFont text-5xl ml-14 mt-2">Front-End Dev</div>
-                    </span>
-                    <span className='flex justify-between mb-24 cursor-pointer' onClick={() => changeImgJumbo('composer')}>
-                        <div className='containerEstKey'>
-                            <div className='containerIntKey'>
-                                <div className='containerLetter rainbow rainbow_text_animated'>W</div>
-                            </div>
-                        </div>
-                        <div className="textVerticalAlign titleFont text-5xl ml-14 mt-2">Composer</div>
-                    </span>
-                    <span className='flex justify-between' onClick={() => changeImgJumbo('photographer')}>
-                        <div className='containerEstKey'>
-                            <div className='containerIntKey'>
-                                <div className='containerLetter rainbow rainbow_text_animated pl-1'>E</div>
-                            </div>
-                        </div>
-                        <div className="textVerticalAlign ml-12 titleFont text-5xl ml-14 mt-2">Photographer</div>
-                    </span>
+            <div className='flex justify-between w-3/4 center-div'>
+                <div className="w-1/3 brightness-50 hover:brightness-100">
+                    <img src={frntlg} className="imageWidth" alt="frn" />
                 </div>
-                {imgHome}
+                <div className="w-1/3 brightness-50 hover:brightness-100">
+                    <img src={cmplg} className="imageWidth" alt="cmp" />    
+                </div>
+                <div className="w-1/3 brightness-50 hover:brightness-100">
+                    <img src={ptglg} className="imageWidth"alt="ptg" />
+                </div>
             </div>
         </div>
     )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,8 @@ import './Header.css'
 
 function Header() {
     return (
-        <nav className="flex justify-between w-3/4 center-div border-b-4 header__borderRadius">
-            <div className=' text-teal-500 w-1/3 flex-none header__textPositionTitle text titleFont text-6xl'>Sebastiano Lanfranchi</div>
+        <nav className="flex justify-between w-3/4 center-div border-b-4 header__borderRadius m-8">
+            <div className='text-zinc-400 w-1/3 flex-none header__textPositionTitle text titleFont text-6xl'>Sebastiano Lanfranchi</div>
             <div className="flex justify-around header__textPosition w-3/12">
                 <span>Front-End Dev</span>
                 <span>Composer</span>
