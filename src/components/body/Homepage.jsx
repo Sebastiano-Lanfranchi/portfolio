e13ba7ba2293b21c8d75b525965123b6fc03eb7d
Sebastiano Lanfranchi <sebastiano.lanfranchi@ff3d.com>
2022-04-28 12:59:58 +0200

    modifiche click image

--- src/components/body/Homepage.jsx
+++ src/components/body/Homepage.jsx
@@ -6,31 +6,74 @@ import React, { useState } from 'react';
 
 function Homepage() {
 
-    const [imgHome, setImgHome] = useState(<div className='max-w-xl relative left-1/4 bottom-24'><img src={frntlg} alt="frn" /></div>);
+    const [frontElem, setFrontElem] = useState(false);
+    const [composerElem, setComposerElem] = useState(false);
+    const [photographerElem, setPhotographerElem] = useState(false);
+    const [isSelect, setSelect] = useState(true);
 
-    function changeImgJumbo(item) {
-        if (item === 'dev') {
-            return setImgHome(<div className='max-w-xl relative left-1/4 bottom-24'><img src={frntlg} alt="frn" /></div>)
-        } else if (item === 'composer') {
-            return setImgHome(<div className='max-w-xl relative left-1/4 bottom-24'><img src={cmplg} alt="cmp" /></div>)
+    function onOfHomepage(page) {
+        if (page) {
+            setSelect(false)
+            switch (page) {
+                case 'frn':
+                    setFrontElem(true)
+                    break;
+                case 'cmp':
+                    setComposerElem(true)
+                    break;
+                case 'pht':
+                    setPhotographerElem(true)
+                    break;
+            }
         } else {
-            return setImgHome(<div className='max-w-xl relative left-1/4 bottom-24'><img src={ptglg} alt="ptg" /></div>)
+            if (isSelect)
+                return setSelect(false)
+            else {
+                setFrontElem(false);
+                setComposerElem(false);
+                setPhotographerElem(false);
+                return setSelect(true)
+            }
         }
     }
 
     return (
         <div className="homepagePosition w-full" >
-            <div className='flex justify-between w-3/4 center-div'>
-                <div className="w-1/3 brightness-50 hover:brightness-100">
-                    <img src={frntlg} className="imageWidth" alt="frn" />
+            {isSelect &&
+                <div>
+                    <div className='flex justify-between w-1/2 center-div pb-20'>
+                        <div name="prima colonna">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. At exercitationem enim eius laudantium dolores nam asperiores totam inventore.
+                            Dolorum repellendus voluptates quia vitae placeat soluta odit! Alias nihil amet quod!
+                        </div>
+                        <div name="seconda colonna">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi mollitia animi harum earum odio, sunt praesentium optio, nesciunt tenetur totam
+                            nulla explicabo illum velit iure magni, dolorem obcaecati? Totam, repellendus?
+                        </div>
+                    </div>
+                    <div className='flex justify-between w-3/4 center-div'>
+                        <div className="w-1/3 brightness-50 hover:brightness-100 cursor-pointer" onClick={() => onOfHomepage('frn')}>
+                            <img src={frntlg} className="imageWidth" alt="frn" />
+                        </div>
+                        <div className="w-1/3 brightness-50 hover:brightness-100 cursor-pointer" onClick={() => onOfHomepage('cmp')}>
+                            <img src={cmplg} className="imageWidth" alt="cmp" />
+                        </div>
+                        <div className="w-1/3 brightness-50 hover:brightness-100 cursor-pointer" onClick={() => onOfHomepage('pht')}>
+                            <img src={ptglg} className="imageWidth" alt="ptg" />
+                        </div>
+                    </div>
                 </div>
-                <div className="w-1/3 brightness-50 hover:brightness-100">
-                    <img src={cmplg} className="imageWidth" alt="cmp" />    
-                </div>
-                <div className="w-1/3 brightness-50 hover:brightness-100">
-                    <img src={ptglg} className="imageWidth"alt="ptg" />
-                </div>
-            </div>
+            }
+            {frontElem &&
+                <div>Sono il front-end developer</div>
+            }
+            {composerElem &&
+                <div>Sono il compositore</div>
+            }
+            {photographerElem &&
+                <div>Sono il fotografo</div>
+            }
+            <button onClick={() => onOfHomepage(null)}>prova</button>
         </div>
     )
 }
