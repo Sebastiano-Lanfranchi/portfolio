1a6c72774ad515edf35539f020b2939bac584251
Sebastiano Lanfranchi <sebastiano.lanfranchi@ff3d.com>
2022-05-02 16:07:34 +0200

    change title and border

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,8 +5,8 @@ function Header() {
 
     const store = useStore();
     return (
-        <nav className="flex justify-between w-3/4 center-div border-b-4 header__borderRadius pt-4">
-            <div className='text-zinc-400 w-1/3 flex-none header__textPositionTitle text titleFont text-6xl'>Sebastiano Lanfranchi</div>
+        <nav className="flex justify-between w-3/4 center-div border-b-4 header__borderRadius pt-4 grey_text_animated ">
+            <div className='w-1/3 flex-none header__textPositionTitle text titleFont text-6xl pt-3 font-bold text-white'>Sebastiano Lanfranchi</div>
             <div className="flex justify-around header__textPosition w-3/12 text-white">
                 <div className='hover:blur-sm cursor-pointer duration-300'  onClick={() => store.onOfHomepage('frn')}>Front-End Dev</div>    
                 <div className='hover:blur-sm cursor-pointer duration-300' onClick={() => store.onOfHomepage('cmp')}>Composer</div>
