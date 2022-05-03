1cab83d0717ce39b7cfd47071a36c6a340299c7c
Sebastiano Lanfranchi <sebastiano.lanfranchi@ff3d.com>
2022-05-03 14:58:18 +0200

    pagina frontend finita

--- src/components/body/Homepage.jsx
+++ src/components/body/Homepage.jsx
@@ -4,28 +4,44 @@ import cmplg from '../../cmplg.jfif';
 import ptglg from '../../ptglg.jpg';
 import React from 'react';
 import useStore from '../services/store';
+import { saveAs } from "file-saver";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub, faSpotify, faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin, faGithub, faSpotify, faInstagram, faFacebook, faChrome } from "@fortawesome/free-brands-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons"
 
 
 function Homepage() {
 
     const store = useStore();
+    const saveFile = () => {
+        saveAs(
+            "https://drive.google.com/file/d/1gtMpBwepYM448ueL9JumlFlQZYayQJQY/view?usp=sharing",
+            "cv.pdf"
+        );
+    };
 
     return (
-        <div className="homepagePosition w-full" >
+        <div className="homepagePosition w-full fade-in-left" >
             {store.isSelect &&
                 <div>
-                    <div className='flex justify-between w-1/2 center-div pb-12 text-white'>
+                    <div className='flex justify-between w-1/2 center-div pb-6 text-white'>
                         <div name="prima colonna" className="mr-10">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. At exercitationem enim eius laudantium dolores nam asperiores totam inventore.
-                            Dolorum repellendus voluptates quia vitae placeat soluta odit! Alias nihil amet quod!
+                            Ciao, sono uno sviluppatore Frontend, sviluppo in particolare con
+                            Angular e React ma conosco anche Laravel e Vue.
+                            Lavoro da 1 anno per FF3D.com
                         </div>
                         <div name="seconda colonna">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi mollitia animi harum earum odio, sunt praesentium optio, nesciunt tenetur totam
-                            nulla explicabo illum velit iure magni, dolorem obcaecati? Totam, repellendus?
+                            Oltre all'informatica ho altre passioni, il pianoforte e la fotografia.
+                            Su questo portfolio potrete ascoltare e vedere i miei lavori.
                         </div>
                     </div>
+                    <div className="center-div w-fit pb-6 ">
+                        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center duration-500" onClick={saveFile}>
+                            <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+                            <span>Download CV</span>
+                        </button>
+                    </div>
+
                     <div className='flex justify-between w-3/4 center-div'>
                         <div className="w-1/3  cursor-pointer  mr-10" onClick={() => store.onOfHomepage('frn')}>
                             <img src={frntlg} className="imageWidth brightness-50 hover:brightness-125 duration-500 rounded-3xl" alt="frn" />
@@ -50,15 +66,100 @@ function Homepage() {
                 </div>
             }
             {store.frontElem &&
-                <div>Sono il front-end developer</div>
+                <div>
+                    <div className="w-1/3 center-div text-white">
+                        <p>
+                            L'informatica mi ha sempre incuriosito, già da piccolo avevo un vecchio PC con win 95 da cui tutto è iniziato.
+                            Dopo gli studi da Geometra ho intrapreso gli studi da Programmatore Web con Boolean, 2 mesi dopo lavoravo già per FF3D.com
+                        </p>
+                        <div>Alcuni dei miei lavori:</div>
+                    </div>
+                    <ul className="cards">
+                        <li>
+                            <a href="https://github.com/Sebastiano-Lanfranchi/proj-html-vuejs" className="card">
+                                <img src="https://media-exp1.licdn.com/dms/image/C4E2DAQGOjoIjkqMDoA/profile-treasury-image-shrink_800_800/0/1618602825292?e=2147483647&v=beta&t=OFWR2WLB5KVmPE8t3y3nvn1Diz8x_66AIaLdQf7kjAQ" className="card__image w-full h-full object-cover" alt="" />
+                                <div className="card__overlay">
+                                    <div className="card__header">
+                                        <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                                        <FontAwesomeIcon className="card__thumb" icon={faGithub} /> 
+                                        <div className="card__header-text">
+                                            <h3 className="card__title">Esercitazione template Phox</h3>
+                                            <span className="card__status">25/02/2021</span>
+                                        </div>
+                                    </div>
+                                    <p className="card__description">Esercitazione Vue, copia del template wordpress Phlox</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/Sebastiano-Lanfranchi/vue-boolflix" className="card">
+                                <img src="https://media-exp1.licdn.com/dms/image/C4E2DAQHT0iIU1fgl2Q/profile-treasury-image-shrink_800_800/0/1618602730720?e=2147483647&v=beta&t=nd5rIDlb_VMcjV-heGEb1LmkNLN1NQw4AEuijNphj5w" className="card__image" alt="" />
+                                <div className="card__overlay">
+                                    <div className="card__header">
+                                        <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                                        <FontAwesomeIcon className="card__thumb" icon={faGithub} /> 
+                                        <div className="card__header-text">
+                                            <h3 className="card__title">Boolflix</h3>
+                                            <span className="card__status">15/02/2021</span>
+                                        </div>
+                                    </div>
+                                    <p className="card__description">Esercitazione Vue e Laravel, copia grafica ed esercitazione chiamate api con Axios</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/Sebastiano-Lanfranchi/html-css-spotifyweb" className="card">
+                                <img src="https://media-exp1.licdn.com/dms/image/C4E2DAQHWsFxQWOVChA/profile-treasury-image-shrink_800_800/0/1618603152404?e=2147483647&v=beta&t=2PNAyHOMpW6htFSf5uYhjXjOCEYRMyEc_IWyUuGGEDE" className="card__image" alt="" />
+                                <div className="card__overlay">
+                                    <div className="card__header">
+                                        <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                                        <FontAwesomeIcon className="card__thumb" icon={faGithub} /> 
+                                        <div className="card__header-text">
+                                            <h3 className="card__title">Esercitazione grafica Spotify</h3>
+                                            <span className="card__status">9/12/2020</span>
+                                        </div>
+                                    </div>
+                                    <p className="card__description">Esercitazione Vue e Laravel, copia grafica di Spotify</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://guazzabuglio.com/" className="card">
+                                <img src="https://media-exp1.licdn.com/dms/image/C4D2DAQF8Y9btsUNrcw/profile-treasury-image-shrink_800_800/0/1651572014140?e=2147483647&v=beta&t=2uVnH1VVFvlspEzjQ_uTXWYIqRTuAZUIqc-gQtXV0Nw" className="card__image" alt="" />
+                                <div className="card__overlay">
+                                    <div className="card__header">
+                                        <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                                        <FontAwesomeIcon className="card__thumb" icon={faChrome} />
+                                        <div className="card__header-text">
+                                            <h3 className="card__title">Guazzabuglio</h3>
+                                            <span className="card__status">20/10/2021</span>
+                                        </div>
+                                    </div>
+                                    <p className="card__description">E-commerce di libri, attualmente online e operativo</p>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="w-fit center-div">
+                         <button className="bg-slate-400 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded" onClick={() => store.onOfHomepage(null)}><FontAwesomeIcon icon={faHome} /></button>
+                    </div>
+                   
+                </div>
             }
-            {store.omposerElem &&
-                <div>Sono il compositore</div>
+            {store.composerElem &&
+                <div>
+                    <div>Sono il compositore</div>
+                    <button className="bg-slate-400 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded" onClick={() => store.onOfHomepage(null)}>prova</button>
+                </div>
+
             }
             {store.photographerElem &&
-                <div>Sono il fotografo</div>
+                <div>
+                    <div>Sono il fotografo</div>
+                    <button className="bg-slate-400 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded" onClick={() => store.onOfHomepage(null)}>prova</button>k
+                </div>
+
             }
-            <button onClick={() => store.onOfHomepage(null)}>prova</button>
         </div>
     )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
 
     const store = useStore();
     return (
-        <nav className="flex justify-between w-3/4 center-div border-b-4 header__borderRadius pt-4 grey_text_animated ">
+        <nav className="flex justify-between w-3/4 center-div border-b-4 header__borderRadius pt-4 fade-in-left">
             <div className='w-1/3 flex-none header__textPositionTitle text titleFont text-6xl pt-3 font-bold text-white'>Sebastiano Lanfranchi</div>
             <div className="flex justify-around header__textPosition w-3/12 text-white">
                 <div className='hover:blur-sm cursor-pointer duration-300'  onClick={() => store.onOfHomepage('frn')}>Front-End Dev</div>    
